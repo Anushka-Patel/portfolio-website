f202b88f8942737b38a0d0b55bbdb3c784849993
Anushka-Patel <143583639+Anushka-Patel@users.noreply.github.com>
2024-05-30 17:14:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let navbar= document.getElementsByClassName('navbar');
+let mainContainer = document.getElementsByClassName('main-container');
+let btn3=document.getElementsByClassName(".btn3");
+let box=document.getElementsByClassName(".box");
+let sidebar=document.getElementsByClassName('.sidebar');
+
+function myfunction(){
+    let btn=document.getElementsByClassName('.download CV');
+        confirm('your CV has been downloaded');
+        
+}
+
+function myfunction(){
+let btn1=document.getElementById('.Hiere Me');
+    confirm('Please send Your Resume');
+}
+
+let popup =document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
